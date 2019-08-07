39a5276fa5528a57e67304390319d50343dbebf8
Gensuke Toshiro <toshigem@gmail.com>
2019-08-07 16:19:02 +0900

    Enrich log fields

--- log/log.go
+++ log/log.go
@@ -5,6 +5,7 @@ import (
     "context"
     "regexp"
     "net/http"
+    "mime/multipart"
     log "github.com/sirupsen/logrus"
     "github.com/gorilla/mux"
     "github.com/openrm/module-tracing-golang"
@@ -28,6 +29,33 @@ const (
     messageRequestHandled = "tracing: request handled"
 )
 
+func parseMultipartForm(form *multipart.Form) map[string]interface{} {
+    parsedForm := make(map[string]interface{}, len(form.File))
+
+    // the content of form.Value is already set in http.Request.Form
+    for k, vs := range form.File {
+        files := make([]map[string]interface{}, len(vs))
+        for i, f := range vs {
+            files[i] = map[string]interface{} {
+                "filename": f.Filename,
+                "headers": f.Header,
+                "size": f.Size,
+            }
+        }
+        parsedForm[k] = files
+    }
+
+    return parsedForm
+}
+
+func parseCookies(cookies []*http.Cookie) []string {
+    serialized := make([]string, len(cookies))
+    for i, cookie := range cookies {
+        serialized[i] = cookie.String()
+    }
+    return serialized
+}
+
 func Handler(logger log.FieldLogger) mux.MiddlewareFunc {
     return func(handler http.Handler) http.Handler {
         return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -46,7 +74,13 @@ func Handler(logger log.FieldLogger) mux.MiddlewareFunc {
             entry = entry.WithFields(log.Fields{
                 "method": r.Method,
                 "protocol": r.Proto,
-                "uri": r.RequestURI,
+                "url": r.RequestURI,
+                "remoteAddress": r.RemoteAddr,
+                "hostname": r.Host,
+                "referer": r.Referer(),
+                "userAgent": r.UserAgent(),
+                "cookies": parseCookies(r.Cookies()),
+                "contentLength": r.ContentLength,
             })
 
             if sp, ok := r.Context().Value(tracing.SpanContextKey).(*tracing.Span); ok {
@@ -62,10 +96,18 @@ func Handler(logger log.FieldLogger) mux.MiddlewareFunc {
             l := newResponseLogger(w)
             handler.ServeHTTP(l, r.WithContext(ctx))
 
+            if r.Form != nil {
+                entry = entry.WithField("form", r.Form)
+            }
+
+            if r.MultipartForm != nil {
+                entry = entry.WithField("files", parseMultipartForm(r.MultipartForm))
+            }
+
             entry = entry.WithFields(log.Fields{
-                "responseTime": time.Since(start),
+                "responseTime": float64(time.Since(start).Nanoseconds()) / 1e6, // ms
                 "status": l.status,
-                "contentLength": l.size,
+                "responseHeaders": l.Header(),
             })
 
             entry.Info(messageRequestHandled)
